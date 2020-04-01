5d905572cb41f3c6943d85cf2ec03977959eadba
Bushra-B <bushraxbil@gmail.com>
2020-04-01 06:58:01 +0300

    lab03

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 'use strict';
 var keywrods = [];
 var animals = [];
+var titlesArr = [];
+var hornsArr =[];
+// let hornAnimal;
 // C.F:
 function HornAnimals (title, img, description, keyword, horns) {
   this.title = title;
@@ -8,55 +11,93 @@ function HornAnimals (title, img, description, keyword, horns) {
   this.description =description;
   this.keyword = keyword;
   this.horns = horns;
+  this.addClass = keyword;
   animals.push(this);
+  titlesArr.push(title);
+  hornsArr.push(horns);
 }
 // getting the data from JSON file:
-$.get('./data/page-1.json').then(data => {
-  data.forEach((animal)=>{ //get each object from the array in JSON file and create a new object for it using C.F
-    let hornAnimal = new HornAnimals(animal.title, animal.image_url, animal.description, animal.keyword, animal.horns);
-    hornAnimal.render();
-    hornAnimal.addOption();
+function pageOne() {
+  $.get('./data/page-1.json').then(data => {
+    data.forEach((animal)=>{ //get each object from the array in JSON file and create a new object for it using C.F
+      let hornAnimal = new HornAnimals(animal.title, animal.image_url, animal.description, animal.keyword, animal.horns);
+      hornAnimal.render();
+      hornAnimal.addOption();
     // selectOption();
+    });
   });
+}
+function pageTwo(){
+  $.get('./data/page-2.json').then(data => {
+    data.forEach((animal)=>{ //get each object from the array in JSON file and create a new object for it using C.F
+      let hornAnimal = new HornAnimals(animal.title, animal.image_url, animal.description, animal.keyword, animal.horns);
+      hornAnimal.render();
+      hornAnimal.addOption();
+    // selectOption();
+    });
+  });
+}
+pageOne();
+// page 1 event:
+$('#pg1').on('click', function() {
+  pageOne();
+  console.log('clikc pg1');
 });
-
+// page 2 event:
+$('#pg2').on('click', function(){
+  $('section').removeData();
+  $('section').removeAttr();
+  pageTwo();
+  console.log('pg2');
+});
 // prototype to render the data on html:
 HornAnimals.prototype.render = function() {
-  let photoTemplate = $('.photo-template').clone();
-  photoTemplate.removeClass('photo-template');
-  photoTemplate.find('h2').text(this.title);
-  photoTemplate.find('img').attr('src', this.image_url);
-  photoTemplate.find('p').text(this.description);
-  $('main').append(photoTemplate);
+  // let photoTemplate = $('.photo-template').clone();
+  // photoTemplate.removeClass('photo-template');
+  // photoTemplate.addClass(`${this.keyword}`);
+  // photoTemplate.find('h2').text(this.title);
+  // photoTemplate.find('img').attr('src', this.image_url);
+  // photoTemplate.find('p').text(this.description);
+  // $('main').append(photoTemplate);
+  let mustacheTemplate = $('#mustache-template').html();
+  let htmlPage = Mustache.render(mustacheTemplate,this);
+  $('main').append(htmlPage);
+  // mustacheTemplate.addClass(`${this.keyword}`);
 };
 
 // adding dropdown menu options dynamically:
 HornAnimals.prototype.addOption = function(){
   if (keywrods.includes(this.keyword) === false) {
-    $('select').append(`<option>${this.keyword}</option>`);
+    $('#filter').append(`<option>${this.keyword}</option>`);
     keywrods.push(this.keyword);
   }
 };
-
-// showing images according to selected option:
-// HornAnimals.prototype.selectOption = function(){
-//   $('select').change(function(){
-//     $('section').hide();
-//     console.log($(this).val());
-//     console.log(this);
-//   });
-// };
-
 function selectOption () {
-  $('select').change(function(){
+  $('#filter').change(function(){
     $('section').hide();
-    // console.log($(this).val());
     for (let i=0; i<animals.length; i++) {
       if (animals[i].keyword === $(this).val()) {
-        animals[i].render();
-        $('section').show();
+        $(`.${animals[i].keyword}`).fadeIn(700);
       }
     }
   });
 }
 selectOption();
+function sortAnimals () {
+  $('#sort').change(function(){
+    if ('title' === $(this).val()) {
+      animals.sort((a, b) => {
+        a.title > b.title;
+      });
+      console.log(animals);
+    }
+    else if ('horns' === $(this).val()){
+      animals.sort((a, b) => {
+        a.horns > b.horns;
+      });
+      console.log(animals);
+    }
+  });
+}
+sortAnimals ();
+
